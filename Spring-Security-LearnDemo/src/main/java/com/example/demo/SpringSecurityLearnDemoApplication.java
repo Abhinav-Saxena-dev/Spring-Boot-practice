394d7ba0ca0f56e6998fc175ec2e66d6b53ad916
Abhinav-Saxena-dev <abhinavsaxena148@gmail.com>
2022-03-20 15:16:31 +0530

    adding new demo project to learn spring security

---
+++ Spring-Security-LearnDemo/src/main/java/com/example/demo/SpringSecurityLearnDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityLearnDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityLearnDemoApplication.class, args);
+	}
+
+}
