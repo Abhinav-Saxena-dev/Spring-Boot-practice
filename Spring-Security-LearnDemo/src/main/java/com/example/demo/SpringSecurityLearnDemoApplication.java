f89da4583c127ffec3fbe6b1aac6b0b1463153d9
Abhinav-Saxena-dev <abhinavsaxena148@gmail.com>
2022-03-23 00:48:30 +0530

    adding data base functionality to security form

--- Spring-Security-LearnDemo/src/main/java/com/example/demo/SpringSecurityLearnDemoApplication.java
+++ Spring-Security-LearnDemo/src/main/java/com/example/demo/SpringSecurityLearnDemoApplication.java
@@ -1,13 +1,24 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringSecurityLearnDemoApplication {
+import com.example.demo.dao.UserRepository;
 
+@SpringBootApplication
+public class SpringSecurityLearnDemoApplication implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityLearnDemoApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		
+	}
 }
--- Spring-Security-LearnDemo/src/main/java/com/example/demo/models/User.java
+++ Spring-Security-LearnDemo/src/main/java/com/example/demo/models/User.java
@@ -1,6 +1,12 @@
 package com.example.demo.models;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+
+	@Id
 	String username;
 	String password;
 	String email;
